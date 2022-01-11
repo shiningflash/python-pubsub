45630dde2ccfffdd4b7ad63986af544c14ecb6e1
Amirul Islam <amirulislamalmamun@gmail.com>
2022-01-11 22:21:12 +0600

    smart home publisher added

---
+++ smart_home/publisher.py
@@ -0,0 +1,21 @@
+import os
+
+from google.cloud import pubsub_v1
+
+credentials_path = '/Users/mac/python-pubsub/smart_home/mysticcurve_privatekey.json'
+os.environ['GOOGLE_APPLICATION_CREDENTIALS'] = credentials_path
+
+publisher = pubsub_v1.PublisherClient()
+topic_path = 'projects/mystic-curve-337915/topics/env-sensors'
+
+
+def send_clearance():
+    data = "The house is safe. All good."
+    data = data.encode('utf-8')
+    
+    future = publisher.publish(topic_path, data)
+    print('Published message id', future.result())
+
+
+if __name__ == '__main__':
+    send_clearance()
